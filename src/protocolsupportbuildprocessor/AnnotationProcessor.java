f5106ed0813dedc926970074c2ea2fa38f77d7bc
Shevchik <shev4ik.den@gmail.com>
2021-11-16 11:37:59 +0300

    Support updating existing preload list

--- src/protocolsupportbuildprocessor/AnnotationProcessor.java
+++ src/protocolsupportbuildprocessor/AnnotationProcessor.java
@@ -1,11 +1,13 @@
 package protocolsupportbuildprocessor;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -39,15 +41,31 @@ public class AnnotationProcessor extends AbstractProcessor {
 	@Override
 	public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment env) {
 		try {
-			File file = new File(processingEnv.getOptions().get(preloadFilePathOptionName));
-			file.getParentFile().mkdirs();
-			Files.write(
-				file.toPath(),
+			Path path = Paths.get(processingEnv.getOptions().get(preloadFilePathOptionName));
+			if (Files.exists(path)) {
+				//load existing entries to list
+				Set<String> list = new LinkedHashSet<>(Files.readAllLines(path));
+				//remove entries for classes that aren't annotated
+				env.getRootElements().stream()
+				.filter(element -> element.getAnnotation(Preload.class) == null)
+				.map(element -> ((TypeElement) element).getQualifiedName())
+				.forEach(annotatedName -> list.remove(annotatedName.toString()));
+				//add entries for classes that are annotated
 				env.getElementsAnnotatedWith(Preload.class).stream()
 				.map(element -> ((TypeElement) element).getQualifiedName())
-				.collect(Collectors.toList()),
-				StandardOpenOption.CREATE
-			);
+				.forEach(annotatedName -> list.add(annotatedName.toString()));
+				//write entries
+				Files.write(path, list, StandardOpenOption.TRUNCATE_EXISTING);
+			} else {
+				Files.createDirectories(path.getParent());
+				Files.write(
+					path,
+					env.getElementsAnnotatedWith(Preload.class).stream()
+					.map(element -> ((TypeElement) element).getQualifiedName())
+					.collect(Collectors.toList()),
+					StandardOpenOption.CREATE
+				);
+			}
 		} catch (IOException e) {
 			processingEnv.getMessager().printMessage(Kind.ERROR, "Unable to write generated preload list: " + e.getMessage());
 		}
